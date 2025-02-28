9299d2a4c4b2744078ef0f75f9f4498312da48db
Pedro de Barros Alves <pedro.de.barros.alves@gmail.com>
2025-02-28 10:07:16 -0300

    fix calculo e add classificacao

--- src/components/Formulario/Formulario.jsx
+++ src/components/Formulario/Formulario.jsx
@@ -1,11 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from './Formulario.module.css';
 
 const Formulario = () => {
 
     const [altura, setAltura] = useState(0);
     const [peso, setPeso] = useState(0);
-    const imc = peso/altura;
+    const [imc, setImc] = useState(0);
+    const [classificacao, setClassificacao] = useState('');
+
+    
+    useEffect(() => {
+        calculaIMC();
+    }, [peso, altura, imc])
+    
+    
+    const calculaIMC = () => {
+        if(altura < 2.72 || peso < 595) {
+            setImc(peso/(altura*altura));
+            console.log(imc);
+            if(imc < 18.5) {
+                setClassificacao('Magreza');
+            } else if(18.5 < imc && imc < 24.9) {
+                setClassificacao('Normal');
+            } else if(25.0 < imc && imc < 29.9) {
+                setClassificacao('Sobrepeso');
+            } else if(30.0 < imc && imc < 39.9) {
+                setClassificacao('Obesidade');
+            } else if(imc > 40.0) {
+                setClassificacao('Obesidade Grave');
+            }
+        } else {
+            setImc('Insira valores reais em Kg e em m');
+            setClassificacao('');
+        }
+        
+    }
     
     return (
         <div>
@@ -16,7 +45,8 @@ const Formulario = () => {
             {altura != 0 && peso != 0 && (
                 <>
                     <h3 className={styles.resultado}>Seu IMC é:</h3>
-                    <h3 className={styles.resultado}>{imc}</h3>
+                    <h3 className={styles.resultado}>{imc.toFixed(2)}</h3>
+                    <h3 className={styles.resultado}>Classificação: {classificacao}</h3>
                 </>
                 )}
         </div>
